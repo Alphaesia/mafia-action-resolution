d26e157c918288a630258e43e99d07fe7ad153d8
CreepaShadowz <creepashadowz@gmail.com>
2018-08-27 21:21:10 +1200

    Did stuff relating to the execution of abilities... I think?

--- abilities.py
+++ abilities.py
@@ -33,19 +33,29 @@ class Ability:
 	def connect_to_targets(self):
 		for action in self.actions:
 			action.target.targetted_by.append(self)
+			if (action.target_ability == "*"):
+				for target_ability in action.target.cast_abilities:
+					target_ability.modified_by.append(self)
 
 
-	def resolve(self):
-		for action in self.actions:
-			if (self.modified_by):
-				pass
-			else:
-				if (action.type == "get"):
+	def resolve(self, *ability_chain):
+		def execute():
+			if (action.type == "get"):
 					action.returned_value = getattr(action.target, action.component)
 					self.return_message += str(self.interpret_results(action))
+					self.success = True
+					self.resolved = True
 
-			self.success = True
-			self.resolved = True
+		if (not ability_chain):
+			print("NO ABILITY CHAIN")
+
+		for action in self.actions:
+			if (self.modified_by):
+				for ability in self.modified_by:
+					if (ability not in ability_chain):
+						ability.resolve()
+			else:
+				execute()
 
 	def interpret_results(self):
 		NotImplemented
@@ -57,10 +67,10 @@ class Action:
 
 		if (isinstance(target, tuple)):
 			self.target = target[0]
-			self.target_action = target[1]
+			self.target_ability= target[1]
 		else:
 			self.target = target
-			self.target_action = None
+			self.target_ability = None
 
 		self.component = component
 
@@ -75,7 +85,7 @@ class Action:
 			"action": self.__class__.__name__,
 			"type": self.type,
 			"target": (self.target.__name__(), self.target),
-			"target_action": (self.target_action.__class__.__name__, self.target_action),
+			"target_ability": (self.target_ability.__class__.__name__, self.target_ability),
 			"component": self.component,
 			"new_value": self.new_value,
 			"returned_value": self.returned_value
--- mafia_action_resolution.py
+++ mafia_action_resolution.py
@@ -12,6 +12,7 @@ def resolve_abilities(players):
 	for player in players:
 		for ability in player.cast_abilities:
 			ability.resolve()
+			print(ability.caster.__name__() + ": " + ability.return_message)
 		PrettyPrinter(indent=4).pprint(player.get_components())
 
 players = []
@@ -23,7 +24,7 @@ PlayerC = player.Player("Zatharel", "mafia", roles.Roleblocker())
 players.extend((PlayerA, PlayerB, PlayerC))
 
 PlayerA.cast(PlayerA.role.abilities["investigate"], (PlayerB, PlayerA))
-PlayerB.cast(PlayerB.role.abilities["investigate"], (PlayerA, PlayerB))
+PlayerB.cast(PlayerB.role.abilities["investigate"], (PlayerA, PlayerC))
 PlayerC.cast(PlayerC.role.abilities["block"], (PlayerA,))
 
 finalise_abilities(players)
